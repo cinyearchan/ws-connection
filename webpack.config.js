d3f3f1482aeb5cb14827251e9e0f5d69b13af64e
cinyearchan <cinyearchan@foxmail.com>
2020-08-26 18:17:17 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const config = {
+    entry: {
+        index: './lib/index.js'
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    optimization: {
+        minimize: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    mode: 'production'
+}
+
+module.exports = config
